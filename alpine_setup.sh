ce4c7eee63d10db0e10b5664f1317a0198e55b03
Matt Visich <mvisich@mvisich.powerchordsystem-12-12-23>
2024-06-07 12:03:25 -0600

    adding more pip packages

--- alpine_setup.sh
+++ alpine_setup.sh
@@ -15,8 +15,7 @@ pip3 install --upgrade pip
 
 python3 -m venv .venv
 source .venv/bin/activate
-python3 -m pip install click
-#-r ./requirements.txt
+python3 -m pip install -r ./requirements.txt
 
 
 # Install required Python packages
