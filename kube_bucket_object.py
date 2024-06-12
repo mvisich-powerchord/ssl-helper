c814eae8b2ca99b4d835174738221a4588f5b982
Matt Visich <mvisich@mvisich.powerchordsystem-12-12-23>
2024-06-12 13:45:00 -0600

    i stink at this

--- kube_bucket_object.py
+++ kube_bucket_object.py
@@ -23,10 +23,10 @@ def get_bucket_name():
     return decodedv2
 
 def list_objects(bucketname):
-    """Lists all the blobs in the bucket."""
+    from google.cloud import storage
+    client = storage.Client()
+        """Lists all the blobs in the bucket."""
     # bucket_name = "your-bucket-name"
-
-    storage_client = storage.Client()
 
     # Note: Client.list_blobs requires at least package version 1.17.0.
     blobs = storage_client.list_blobs(bucketname)
@@ -34,7 +34,6 @@ def list_objects(bucketname):
     # Note: The call returns a response only when the iterator is consumed.
     for blob in blobs:
         print(blob.name)
-    
 
 
 @click.command()
