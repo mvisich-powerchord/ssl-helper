dbf1dbfc6afae397ad97dbdc4d3b4d36f97d6104
Matt Visich <mvisich@mvisich.powerchordsystem-12-12-23>
2024-06-12 13:56:18 -0600

    spaces are hard

--- kube_bucket_object.py
+++ kube_bucket_object.py
@@ -25,16 +25,9 @@ def get_bucket_name():
 def list_objects(bucketname):
     from google.cloud import storage
     client = storage.Client()
-        """Lists all the blobs in the bucket."""
-    # bucket_name = "your-bucket-name"
-
-    # Note: Client.list_blobs requires at least package version 1.17.0.
     blobs = storage_client.list_blobs(bucketname)
-
-    # Note: The call returns a response only when the iterator is consumed.
     for blob in blobs:
         print(blob.name)
-
 
 @click.command()
 #@click.option('--cert-file', prompt='Select a cert', type=click.Choice(['none'] + get_namespaces()), default='all-namespaces')
