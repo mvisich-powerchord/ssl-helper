66a5e2ef89080ae9679773eab944f0c28957bd65
Matt Visich <mvisich@mvisich.powerchordsystem-12-12-23>
2024-06-27 09:59:20 -0600

    adding backup secrets

--- kube_secret_update_bucket.py
+++ kube_secret_update_bucket.py
@@ -53,6 +53,33 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
 
     print(f"Blob {source_blob_name} downloaded to {destination_file_name}.")
 
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
 def parse_secret_name(secret_name_str):
     return json.loads(secret_name_str.replace("'", '"'))
 
@@ -216,6 +243,12 @@ def update_secret_bucket(certfile,secretname,password):
         click.echo("End date is in the past. Exiting.")
         return
 
+
+    click.echo("Backing up existing secret...")
+    restore_command = backup_secret(secret_name, namespace)
+    click.echo("Secret backed up. You can restore it using the following command:")
+    click.echo(restore_command)
+    
     #click.echo(f"Size of PFX file: {file_size} bytes")
 
 
